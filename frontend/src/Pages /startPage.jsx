ce21b7eaf0ccf2afc13ade7c3466e77375e4ceb2
GianlucaMarc <gian.marotta@yahoo.com>
2022-05-12 11:21:14 +0200

    StartPage

---
+++ frontend/src/Pages /startPage.jsx
@@ -0,0 +1,14 @@
+const StartPage = () => {
+  return (
+    <main>
+      <h1>Spend Smarter</h1>
+      <h1>Save More</h1>
+      <button>Get Started</button>
+      <p>
+        Already Have An Account? <link rel="stylesheet" href="Log In" />
+      </p>
+    </main>
+  );
+};
+
+export default StartPage;
