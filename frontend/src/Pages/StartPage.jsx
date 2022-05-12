8a25219a87a9d72a2b1e469982abaabec4a447e6
MarzioCostantini <costantinimarzio@gmail.com>
2022-05-12 13:17:06 +0200

    Again css basic

--- frontend/src/Pages/StartPage.jsx
+++ frontend/src/Pages/StartPage.jsx
@@ -6,7 +6,7 @@ const StartPage = () => {
       <img src="./img/Man.png" alt="" />
       <h1>Spend Smarter</h1>
       <h1>Save More</h1>
-      <Link className="btn-blue" to="/">
+      <Link className="btn-blue" to="/login">
         Get Started
       </Link>
 
