db8cd26438770dca015dca3be7109756f2cce7a9
GianlucaMarc <gian.marotta@yahoo.com>
2022-05-13 16:35:47 +0200

    TransactionDetails

---
+++ frontend/src/Pages/TransactionDetailsExpense.jsx
@@ -0,0 +1,48 @@
+import "./TransactionDetailsExpense.css";
+
+const TransactionDetailsExpense = () => {
+  return (
+    <main>
+      <section>
+        <h1>Transaction details</h1>
+      </section>
+
+      <article>
+        <h2>Transaction details</h2>
+
+        <div className="test">
+          <div>
+            <div>
+              <p>Status</p>
+              <p>To</p>
+              <p>Time</p>
+              <p>Date</p>
+            </div>
+            <div>
+              <hr />
+              Spending
+              <hr />
+            </div>
+            <div>Total</div>
+          </div>
+          <div>
+            <div>
+              <p>Status</p>
+              <p>To</p>
+              <p>Time</p>
+              <p>Date</p>
+            </div>
+            <div>
+              <hr />
+              Spending
+              <hr />
+            </div>
+            <div>Total</div>
+          </div>
+        </div>
+      </article>
+    </main>
+  );
+};
+
+export default TransactionDetailsExpense;
---
+++ frontend/src/Pages/TransactionDetailsIncome.jsx
@@ -0,0 +1,44 @@
+import "./TransactionDetailsIncome.css";
+
+const TransactionDetailsIncome = () => {
+  return (
+    <main>
+      <section>
+        <h1>Transaction details</h1>
+      </section>
+
+      <div className="test">
+        <div>
+          <div>
+            <p>Status</p>
+            <p>From</p>
+            <p>Time</p>
+            <p>Date</p>
+          </div>
+          <div>
+            <hr />
+            Earnings
+            <hr />
+          </div>
+          <div>Total</div>
+        </div>
+        <div>
+          <div>
+            <p>Status</p>
+            <p>From</p>
+            <p>Time</p>
+            <p>Date</p>
+          </div>
+          <div>
+            <hr />
+            Earnings
+            <hr />
+          </div>
+          <div>Total</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default TransactionDetailsIncome;
