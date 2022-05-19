727621435a920fa304afafd3dfd7fedf5309ad7e
MarzioCostantini <costantinimarzio@gmail.com>
2022-05-19 14:51:56 +0200

    Statistik Css

--- frontend/src/Components/Diagramm.jsx
+++ frontend/src/Components/Diagramm.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import { Line } from "react-chartjs-2";
-//import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS } from "chart.js/auto";
 
 const Diagramm = ({chartData}) => {
+  
   return (
     <section>
      <article>
--- frontend/src/Pages/Statistik.jsx
+++ frontend/src/Pages/Statistik.jsx
@@ -1,39 +1,51 @@
 import Navbar from "../Components/Navbar";
 import HeaderLine from "../Components/HeaderLine";
 
-
 import "./Statistik.css";
 import TopAusgaben from "../Components/TopAusgaben";
 import Diagramm from "../Components/Diagramm";
 import { useState } from "react";
 
 import { UserData } from "../Data";
-
+// import { ContextProvider } from "react-chat-engine";
 
 const Statistik = () => {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
 
-const [userData, setUserData] = useState({
-  labels: UserData.map((data) => data.year),
-  datasets: [{
-    label: "Einnahmen und Ausgaben",
-    data: UserData.map((data) => data.userGain),
-    backgroundColor: [
-      '#2b47fc',
+
+
+    datasets: [
+      {
+        label: "Ausgaben und Einnahmen",
+    
+        data: UserData.map((data) => data.userGain),
+        fill: true,
+        backgroundColor: "#2b47fc42",
+        borderColor: "#2b47fc",
+        tension: 0.4,
+        radius: 5,
+        hoverRadius: 10,
+        pointBackgroundColor: "#2b47fc",
+        borderWidth: 2,
+      
+       
+      },
     ],
-    borderColor: [
-      '#2b47fc',
-  ],
-  borderWidth: 2
-  }]
-})
-
-  return (
-    <main className="statistik">
-      <HeaderLine title="Statistik" />
-        <Diagramm chartData={userData}/>
+    
+  });
 
   
-      <TopAusgaben/>
+
+  
+  return (
+    
+    <main className="statistik">
+      <HeaderLine title="Statistik" />
+
+      <Diagramm chartData={userData} />
+
+      <TopAusgaben />
       <Navbar />
     </main>
   );
