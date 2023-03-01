b5a38cbdc27c8fb841b6138107eef0fdbde5daca
iderdiyok <37453696+iderdiyok@users.noreply.github.com>
2023-03-01 11:09:58 +0100

    Update Diagramm.jsx

--- frontend/src/Components/Diagramm.jsx
+++ frontend/src/Components/Diagramm.jsx
@@ -2,7 +2,6 @@ import "./Diagramm.css"
 import React from "react";
 
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
 
 const Diagramm = ({chartData}) => {
   
